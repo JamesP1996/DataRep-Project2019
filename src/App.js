8d7c647edf0863cbae80ab9b0f95907674932f1f
James Porter <43405677+JamesP1996@users.noreply.github.com>
2019-11-19 21:38:12 +0000

    cc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Header, Read } from "./Components/read";
-import { Footer, Create } from "./Components/create";
+import {  Read } from "./components/read";
+import { Create } from "./components/create";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Home } from './Components/home';
+import { Home } from './components/home';
 import icon from './Assets/navIcon.png'
 
 
@@ -17,7 +17,7 @@ function App() {
 
         <Navbar id="navbar-custom" variant="dark">
           <Nav className="mr-auto">
-            <img src={icon} height="40" width="40"></img>
+            <img src={icon} alt="Game Icon"height="40" width="40"></img>
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/read">Read</Nav.Link>
             <Nav.Link href="/create">Create</Nav.Link>
