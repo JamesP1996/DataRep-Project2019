87a56b40499a90bc8bc4c9c43a8ee3fe5e8736cc
James Porter <43405677+JamesP1996@users.noreply.github.com>
2019-11-19 18:55:36 +0000

    Modified UI And Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header } from "./Components/header";
+import { Footer } from "./Components/footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home } from './Components/home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>G00327095 Project 2019</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+
+        <Navbar id="navbar-custom" variant="dark">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/read">read</Nav.Link>
+            <Nav.Link href="/create">create</Nav.Link>
+
+
+
+          </Nav>
+        </Navbar>
+        <br />
+        <Switch>
+
+          <Route path='/' component={Home} exact />
+          <Route exact path='/create' component={Footer} />
+          <Route path='/read' component={Header} />
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Home extends Component {
+    render() {
+        return (
+
+            <h1>Geek Reviews!</h1>
+
+
+        );
+    }
+
+}
