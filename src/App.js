4e498a9c79cf5569a63b82dec7522f6f91207a9b
James Porter <43405677+JamesP1996@users.noreply.github.com>
2019-11-19 19:41:14 +0000

    Modifications

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from './Components/home';
+import icon from './Assets/navIcon.png'
 
 
 function App() {
@@ -16,15 +17,16 @@ function App() {
 
         <Navbar id="navbar-custom" variant="dark">
           <Nav className="mr-auto">
+            <img src={icon} height="40" width="40"></img>
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/read">read</Nav.Link>
-            <Nav.Link href="/create">create</Nav.Link>
+            <Nav.Link href="/read">Read</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
 
 
 
           </Nav>
         </Navbar>
-        <br />
+
         <Switch>
 
           <Route path='/' component={Home} exact />
--- src/Components/home.js
+++ src/Components/home.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 
+
+const bg = {
+    background: 'maroon',
+    color: 'white'
+};
+
 export class Home extends Component {
+
+
     render() {
         return (
+            <div id="parent" >
+                <h1>Geek Reviews!</h1>
+                <h8>Welcome to Geek Reviews, Your Latest Game Reviews!</h8>
+                <div style={bg}>
 
-            <h1>Geek Reviews!</h1>
 
+                </div>
+            </div>
 
         );
     }
