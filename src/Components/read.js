18d08b535d095faa1ce2f103974be6e5d58845e6
James Porter <43405677+JamesP1996@users.noreply.github.com>
2019-11-21 17:15:29 +0000

    Updated Read Component To Take in Game Items

--- src/Components/read.js
+++ src/Components/read.js
@@ -1,12 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
+import Games from './games'
+import { log } from "util";
 
-export class Read extends Component {
+export class Read extends React.Component {
+
+    state = {
+        games: []
+    };
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/api/games')
+            .then((response) => {
+                this.setState({ games: response.data.games })
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+
+
     render() {
         return (
             <div id="read">
                 <h1>Read Component</h1>
+                <Games myGames={this.state.games}></Games>
             </div>
 
         );
     }
 }
+
+export default Read;
